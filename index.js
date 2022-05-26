148e9285d387164af04d5d99751d0f999e3fd0fb
Daniel Gonzalez <danielgnzch@gmail.com>
2022-05-26 20:05:57 +0200

    done

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const fakeApi = require('./data');
 const app = express();
 
 /* Your code goes here */
+app.get('/', (request, res, next) => {
+    res.send('<h1>Hello world</h1>');
+})
+
+app.get('/loan', (request, res, next) => {
+    const filteredStudents = fakeApi.filter((elem) => elem.hasLoan === true);
+    res.json(filteredStudents);
+})
+
+app.get('/score', (request, res, next) => {
+
+    const total = fakeApi.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue.avgScore;
+    }, 0);
+    const avg = (total / fakeApi.length).toFixed(2);
+    res.send(`<h2>The group's average score is ${avg}</h2>`);
+})
+
 
 // Server listening
 app.listen(3000, () => console.log('🚀 My first app listening on port 3000! '));
